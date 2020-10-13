6b828f9a754f3c262a7c7a5bbc0c7c100060153a
Jiang Han <jianghan99@gmail.com>
2020-10-13 23:54:37 +0700

    Creating packages for MVVM architecture. Moving MainActivity.kt to view package.

--- app/src/main/java/com/example/randommenupicker/view/MainActivity.kt
+++ app/src/main/java/com/example/randommenupicker/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.randommenupicker
+package com.example.randommenupicker.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.randommenupicker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
