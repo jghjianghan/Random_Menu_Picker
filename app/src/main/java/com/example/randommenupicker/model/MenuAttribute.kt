8eb6c66e8d5c7b764ca93f32dd2e3f9136a53c5a
Jiang Han <jianghan99@gmail.com>
2020-10-14 23:13:05 +0700

    Creating MenuAttribute.kt as Enum. Will be used to define search and history category.

---
+++ app/src/main/java/com/example/randommenupicker/model/MenuAttribute.kt
@@ -0,0 +1,9 @@
+package com.example.randommenupicker.model
+
+enum class MenuAttribute(val text: String) {
+    NAMA("Nama"),
+    DESKRIPSI("Desc"),
+    BAHAN("Bahan"),
+    TAG("Tag"),
+    RESTO("Resto")
+}
