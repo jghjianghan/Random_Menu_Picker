6283a36a4a9079a018363224ddba14d1f4aee7b5
Jiang Han <jianghan99@gmail.com>
2020-10-15 22:09:02 +0700

    Creating Page.kt, a Enum representation of pages (navigation buttons)

---
+++ app/src/main/java/com/example/randommenupicker/model/Page.kt
@@ -0,0 +1,12 @@
+package com.example.randommenupicker.model
+
+import android.content.res.Resources
+import com.example.randommenupicker.R
+
+enum class Page (val text: String) {
+    HOME("Home"),
+    CARI("Cari"),
+    MENU("Menu"),
+    SETTING("Setting"),
+    EXIT("Exit")
+}
