d4f1e3acdd1e84390c6e43c0360d67cff0fedb5e
Jiang Han <jianghan99@gmail.com>
2020-10-15 13:46:46 +0700

    Creating MenuChooser.kt. Provides utility method to randomly choose menu given a list of menu.

---
+++ app/src/main/java/com/example/randommenupicker/model/MenuChooser.kt
@@ -0,0 +1,31 @@
+package com.example.randommenupicker.model
+
+class MenuChooser () {
+    private var choiceHistory: ArrayList<Menu> = ArrayList()
+
+    private fun trimHistory(historyLimit: Int){
+        val tempHist: ArrayList<Menu> = ArrayList()
+        var limit = if(historyLimit<choiceHistory.size) {historyLimit} else {choiceHistory.size}
+        for ((idx, i) in choiceHistory.withIndex()){
+            if (idx >= (choiceHistory.size-limit)){
+                tempHist.add(i)
+            }
+        }
+        choiceHistory = tempHist
+    }
+
+    fun getMenu(listMenu: ArrayList<Menu>, historyLimit: Int): Menu{
+        var chosen: Menu
+        val availableMenu = ArrayList<Menu>()
+        for (i in listMenu){
+            if (!choiceHistory.contains(i)){
+                availableMenu.add(i)
+            }
+        }
+        chosen = availableMenu[(0 until availableMenu.size).random()]
+
+        choiceHistory.add(chosen)
+        trimHistory(historyLimit)
+        return chosen
+    }
+}
