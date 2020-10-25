e4281ee4bb5270e35a7d837b49757c051b34d133
Jiang Han <jianghan99@gmail.com>
2020-10-25 16:55:54 +0700

    Adding onclick for randomized menu to open menu detail.

--- app/src/main/java/com/example/randommenupicker/view/HomeFragment.kt
+++ app/src/main/java/com/example/randommenupicker/view/HomeFragment.kt
@@ -2,6 +2,8 @@ package com.example.randommenupicker.view
 
 import android.content.Context
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.randommenupicker.R
 import com.example.randommenupicker.databinding.FragmentHomeBinding
+import com.example.randommenupicker.model.Menu
+import com.example.randommenupicker.model.Page
 import com.example.randommenupicker.viewmodel.MainActivityViewModel
 
 class HomeFragment : Fragment() {
@@ -27,12 +31,23 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         viewModel.getRandomChosenMenu().observe(requireActivity(), {
             if (it == null){
+                binding.tvRandomLabel.visibility = View.GONE
+                binding.tvClickHint.visibility = View.GONE
                 binding.tvResult.text = ""
                 Toast.makeText(requireActivity(),getString(R.string.randomEmptyMenu),Toast.LENGTH_LONG).show();
             } else {
-                binding.tvResult.text = it.nama
+                binding.tvRandomLabel.visibility = View.VISIBLE
+                binding.tvClickHint.visibility = View.VISIBLE
+                val spannableString = SpannableString(it.nama)
+                spannableString.setSpan(UnderlineSpan(), 0, it.nama.length, 0)
+                binding.tvResult.text = spannableString
             }
         })
+        binding.tvResult.setOnClickListener{
+            viewModel.setChosenMenu((viewModel.getRandomChosenMenu().value as Menu).idMenu)
+            viewModel.setPage(Page.MENU_DETAIL)
+        }
+
         binding.btnCari.setOnClickListener {
             viewModel.getRandomMenu()
         }
